e814faed1f5ebfb4f142f34e54a3ae678221136b
MFMvanderholst <117632371+MFMvanderholst@users.noreply.github.com>
2024-02-07 11:57:35 +0100

    blog

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\ReactController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,13 +17,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WelcomeController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::resource('chirps', ChirpController::class)
+    ->only(['index', 'store', 'edit', 'update', 'destroy'])
+    ->middleware(['auth', 'verified']);
+
+Route::resource('reacts', ReactController::class)
+    ->only(['index', 'store', 'edit', 'update', 'destroy'])
+    ->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
